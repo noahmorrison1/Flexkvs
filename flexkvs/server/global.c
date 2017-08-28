cbbe9ab178a540d80cb80034a492a608901f7bcb
noahmorrison1 <31417970+noahmorrison1@users.noreply.github.com>
2017-08-28 14:57:10 -0500

    Update global.c

--- flexkvs/server/global.c
+++ flexkvs/server/global.c
@@ -11,16 +11,19 @@ void global_init()
 
 void display(char * src,char *dest,size_t amount)
 {
+    int count = 0;
     size_t key = *((size_t* )src);
-	if(0)
+	if(1)
 	{
 		int i = 0;
+		if(count > 50) return;
 		while(i < amount)
 		{
 			uint8_t s = (uint8_t) *(src + i);
 			uint8_t d = (uint8_t) *(dest + i);
 			if(s != d)
 			{
+				count++;
 				printf("SRC: %d  DEST: %d I: %d    Key :: %lu    ID :: %d \n",s,d,i % 4096,key,rte_lcore_id());
 			}
 			i++;
